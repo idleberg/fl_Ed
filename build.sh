7f935943cb642f62be54b3fceaec195df989a055
idleberg <jan@idleberg.com>
2013-10-04 18:38:34 +0200

    Update files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar bin/yuicompressor.jar --type js dist/fl_view-dev.js -o dist/fl_view.js
-java -jar bin/yuicompressor.jar --type css dist/fl_view-dev.css -o dist/fl_view.css
+java -jar bin/yui.jar --type js dist/fl_view-dev.js -o dist/fl_view.js
+java -jar bin/yui.jar --type css dist/fl_view-dev.css -o dist/fl_view.css
